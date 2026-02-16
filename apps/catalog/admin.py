ccc9292ac7f0ba46d822ff333bd2730bf762802e
brayden-s-haws <haws@hey.com>
2026-02-15 19:43:56 -0700

    Add initial models, migrations, and admin setup for `apps.catalog`

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Schema, Table, Column
 
 # Register your models here.
+admin.site.register(Schema)
+admin.site.register(Table)
+admin.site.register(Column)
--- apps/catalog/models.py
+++ apps/catalog/models.py
@@ -1,3 +1,33 @@
-from django.db import models
+from __future__ import annotations
 
-# Create your models here.
+from django.db import models
+from apps.core.models import TenantAwareModel
+
+class Schema(TenantAwareModel):
+    source = models.ForeignKey('sources.Source', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, default='')
+
+    def __str__(self) -> str:
+        return self.name
+
+class Table(TenantAwareModel):
+    schema = models.ForeignKey(Schema, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    table_type = models.CharField(max_length=255)
+    row_count = models.IntegerField(default=0, null=True)
+
+    def __str__(self) -> str:
+        return f'{self.schema.name}.{self.name}'
+
+class Column(TenantAwareModel):
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    data_type = models.CharField(max_length=255)
+    nullable = models.BooleanField(default=False)
+    primary_key = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f'{self.table.name}.{self.name}'
+
+
